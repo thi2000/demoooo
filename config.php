49a29fe2eb7c9f1187f6fbbdc316fe11f1f4cd4d
thi2000 <62785764+thi2000@users.noreply.github.com>
2020-10-05 10:34:52 +0700

    sua thu
    sáâsá

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 
     $conn = mysqli_connect($servername,$username,$password);
     if(!$conn)
-        echo "Lỗi kết nối" . mysqli_connect_error();
+        echo "sua thử" . mysqli_connect_error();
     else
         echo "<h1 style='color:green;'>Kết nối thành công</h1>";    
 ?>
